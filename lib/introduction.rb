ae6bc2324bc72ac1a63f5499ead684f3ee035fc6
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-14 01:32:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,14 +2,6 @@ def introduction(name)
   puts "Hi my name is #{"name"}."
 end
 
-def introduction_with_language(name, langauage)
-  puts "Hi my name is #{name} and I am learning to program in #{langauage}."
-end
-
-def introduction_with_language_optional(name, langauage = "Ruby")
-  puts "Hi my name is #{name} and I am learning to program in #{langauage}."
-end
-
 
 
 
