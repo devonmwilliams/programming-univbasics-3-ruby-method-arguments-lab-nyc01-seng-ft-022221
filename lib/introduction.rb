f8d97623522e1e2b8f21b30005858367e1464656
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-14 01:30:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-name = Devon
 def introduction(name)
   puts "Hi my name is #{name}."
 end
+introduction("Devon")
 
 def introduction_with_language(name, langauage)
   puts "Hi my name is #{name} and I am learning to program in #{langauage}."
